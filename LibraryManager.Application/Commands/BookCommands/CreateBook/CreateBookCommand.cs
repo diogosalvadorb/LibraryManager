d1ee97e3c1081370426b40147ee6a60f2ccd5d4f
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-12-29 00:01:21 -0300

    controller book e loan, create commands

---
+++ LibraryManager.Application/Commands/BookCommands/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace LibraryManager.Application.Commands.BookCommands.CreateBook
+{
+    public class CreateBookCommand : IRequest<int>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
---
+++ LibraryManager.Application/Commands/LoanCommands/CreateLoan/CreateLoanHandler.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManager.Core.Entities;
+using LibraryManager.Core.Repositories;
+using MediatR;
+
+namespace LibraryManager.Application.Commands.LoanCommands.CreateLoan
+{
+    public class CreateLoanHandler : IRequestHandler<CreateLoanCommand, int>
+    {
+        private readonly ILoanRepository _repository;
+        public CreateLoanHandler(ILoanRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> Handle(CreateLoanCommand request, CancellationToken cancellationToken)
+        {
+            var loan = new Loan(request.BookId, request.UserId);
+
+            await _repository.AddAsync(loan);
+
+            return loan.Id;
+        }
+    }
+}
